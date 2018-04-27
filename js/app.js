d6753b0002b50f7df7821ad2d6fd595091a97edf
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-26 23:56:08 -0400

    started coding basic logic of the game

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@ const $gameBoard = $('#board');
 const $player1 = $('#player1');
 const $player2 = $('#player2');
 const $boxes = $('li.box');
+let placeholder = 'O'
+startGame();
+
+//this is the newMove function
+$boxes.on('click', (e) =>{
+  e.target.innerText = placeholder;
+  nextTurn();//call the nextTurn to change the contents of placeholder
+});
+
+function startGame() {
+  let placeholder = "O";
+}
+
+
+function nextTurn() {
+  if(placeholder == "O") {
+    placeholder = "X";
+  } else {
+      placeholder = "O";
+  }
+}
